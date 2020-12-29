e0565e447187073510c45c1519b284fab016f155
filipelbc <filipelbc@gmail.com>
2020-12-29 15:41:07 -0300

    Add helper script for identity server migrations

---
+++ scripts/add-identity-migrations.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Use this to add the initial migrations for the Identity Server, while not
+# going to production.
+
+set -ex
+
+cd src/Boilerplate.IdentityServer
+
+dotnet ef migrations add InitialMigration --context PersistedGrantDbContext --output-dir Migrations/PersistentGrantStore
+
+dotnet ef migrations add InitialMigration --context ConfigurationDbContext --output-dir Migrations/ConfigurationStore
