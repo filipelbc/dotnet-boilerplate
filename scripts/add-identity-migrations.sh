b91bedabea0a8c4b1b83ae3d9ff4d0ebe66374f2
filipelbc <filipelbc@gmail.com>
2020-12-30 11:53:44 -0300

    Update identity migration script

--- scripts/add-identity-migrations.sh
+++ scripts/add-identity-migrations.sh
@@ -10,3 +10,5 @@ cd src/Boilerplate.IdentityServer
 dotnet ef migrations add InitialMigration --context PersistedGrantDbContext --output-dir Migrations/PersistentGrantStore
 
 dotnet ef migrations add InitialMigration --context ConfigurationDbContext --output-dir Migrations/ConfigurationStore
+
+dotnet ef migrations add InitialMigration --context ApplicationDbContext --output-dir Migrations/IdentityStore
