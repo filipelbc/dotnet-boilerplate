030b6ec83c7ef6325fe9c6cb0d21c72b741ba1fb
filipelbc <filipelbc@gmail.com>
2020-12-30 18:42:19 -0300

    Add vanilla web client

---
+++ samples/VanillaWebClient/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace VanillaWebClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>()
+                        .UseWebRoot("public")
+                        .UseUrls("https://localhost:5100");
+                });
+    }
+}
---
+++ samples/VanillaWebClient/public/app.js
@@ -0,0 +1,96 @@
+Oidc.Log.logger = window.console;
+Oidc.Log.level = Oidc.Log.DEBUG;
+
+const root = window.location.origin;
+
+const config = {
+    authority: "https://localhost:5000/",
+
+    client_id: "interactive-client",
+    client_secret: "secret",
+
+    redirect_uri: root + "/callback.html",
+    post_logout_redirect_uri: root + "/index.html",
+
+    // these two will be done dynamically from the buttons clicked, but are
+    // needed if you want to use the silent_renew
+    response_type: "code",
+    scope: "api-1 openid profile",
+
+    // silent renew will get a new access_token via an iframe
+    // just prior to the old access_token expiring (60 seconds prior)
+    silent_redirect_uri: root + "/silent.html",
+    automaticSilentRenew: true,
+
+    monitorAnonymousSession: true,
+
+    // will revoke (reference) access tokens at logout time
+    revokeAccessTokenOnSignout: true,
+};
+
+var mgr = new Oidc.UserManager(config);
+
+function login() {
+    mgr.signinRedirect();
+}
+
+function logout() {
+    mgr.signoutRedirect();
+}
+
+function renew() {
+    mgr.signinSilent()
+        .then(function () {
+            console.log("Token silently renewed");
+            showTokens();
+        }).catch(function (err) {
+            console.log("Silent renew error:", err);
+        });
+}
+
+function revoke() {
+    mgr.revokeAccessToken()
+        .then(function () {
+            console.log("Access token revoked")
+        }).catch(function (err) {
+            console.log(err);
+        });
+}
+
+const api_endpoint = "https://localhost:5200/Identity";
+
+function call() {
+    mgr.getUser().then(function (user) {
+        if (user) {
+            fetch(api_endpoint, {
+                headers: {
+                    Authorization: "Bearer " + user.access_token,
+                },
+            }).then(function (response) {
+                console.log("Got:", response);
+            });
+        } else {
+            console.log("Not logged in");
+        }
+    });
+}
+
+document.querySelector("#login").addEventListener("click", login, false);
+document.querySelector("#logout").addEventListener("click", logout, false);
+document.querySelector("#renew").addEventListener("click", renew, false);
+document.querySelector("#revoke").addEventListener("click", revoke, false);
+document.querySelector("#call").addEventListener("click", call, false);
+
+function showTokens() {
+    mgr.getUser()
+        .then(function (user) {
+            if (user) {
+                console.log("Logged in:", user);
+            }
+            else {
+                console.log("Not logged in");
+            }
+        });
+}
+
+showTokens();
