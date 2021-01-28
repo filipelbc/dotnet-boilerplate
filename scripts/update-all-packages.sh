0f89c12c2ee27b1e8847aabf1cb3af9436406953
filipelbc <filipelbc@gmail.com>
2021-01-27 21:31:32 -0300

    Bump dependency versions

--- scripts/update-all-packages.sh
+++ scripts/update-all-packages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for p in `find . -name '*.csproj'`
+for p in $(find . -name '*.csproj')
 do
-    grep -o '".*" Version' $p \
+    grep -o '".*" Version' "$p" \
         | sed 's/"\|" Version//g' \
-        | xargs -n1 dotnet add $p package
+        | xargs -n1 dotnet add "$p" package
 done
